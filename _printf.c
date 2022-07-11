07b8f234f14c0d4b04a7c049548ae2ec1604d3b7
SheilaKahwai <sheilakahwai@users.noreply.github.com>
2022-07-11 02:15:31 -0700

    add fuctions to print output according to format

---
+++ _printf.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string containing characters and specifiers
+ * Return: total number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	const char *trav;
+	int (*pfunc)(va_list);
+
+	int count = 0;
+
+	va_start(args, format);
+
+	if (format == NULL)
+		return (-1);
+	for (trav = format; *trav; trav++)
+	{
+		if (*trav == '%')
+		{
+			trav++;
+			pfunc = get_print(*trav);
+			count += pfunc(args);
+		}
+		else
+			count += _putchar(*trav);
+	}
+	va_end(args);
+	return (count);
+}
+
---
+++ print_func.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_char - prints a character
+ * @args: character to print
+ * Return: 1
+ */
+
+int print_char(va_list args)
+{
+	_putchar(va_arg(args, int));
+	return (1);
+}
+
+/**
+ * print_string - prints a string
+ * @args: string to print
+ * Return: number of characters printed
+ */
+
+int print_string(va_list args)
+{
+	int i;
+	char *str;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	return (i);
+}
+
