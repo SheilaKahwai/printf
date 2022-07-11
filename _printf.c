79d24f3483a5655a32b9bfeff9400a32b7cd2560
livylo <livylo@users.noreply.github.com>
2022-07-11 03:50:24 -0700

    correct syntax

--- _printf.c
+++ _printf.c
@@ -17,8 +17,8 @@ int _printf(const char *format, ...)
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	if (format[0] == '%' && format[1] == ' '; && !fomat[2]);
-	return (-1);
+	if (format[0] == '%' && format[1] == ' '; && !format[2])
+		return (-1);
 
 	for (trav = format; *trav; trav++)
 	{
