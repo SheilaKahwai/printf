5937bc2b345b854e97f360c3aee5fb476e2fbf11
livylo <livylo@users.noreply.github.com>
2022-07-11 02:25:53 -0700

    task add

---
+++ _func.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ *prints a character
+ *Returns formatted output
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
--- get_print.c
+++ get_print.c
@@ -5,7 +5,7 @@
  * depending on the conversion specifier passed to _printf
  * @s: character that holds the conversion specifier
  * Description: the function loops through the structs array
- * func_arr[] to find a match between the specifier passed to _printf
+ * specs[] to find a match between the specifier passed to _printf
  * and the first element of the struct, and then the approriate
  * printing function
  * Return: a pointer to the matching printing function
@@ -13,8 +13,7 @@
 
 int (*get_print(char s))(va_list)
 {
-fmt specs[] =
-{
+fmt specs[] = {
 {'c', print_char},
 {'s', print_string},
 {'%', print_percent},
