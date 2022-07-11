6a7f74005d3f4aba45c1c6953151fa4057255e51
livylo <livylo@users.noreply.github.com>
2022-07-11 03:11:47 -0700

    task 0 addedto printf

--- _printf.c
+++ _printf.c
@@ -15,8 +15,11 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (format == NULL)
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
 	for (trav = format; *trav; trav++)
 	{
 		if (*trav == '%')
